e43bc8928d70dbb61e220572203e7d141fdb045b
Herve Donner <hervedonner@gmail.com>
2020-09-02 20:31:10 +0200

    D8AGE-2: Initial commit.

---
+++ eua_theme/tests/features/bootstrap/DrupalContext.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\eua_theme\Behat;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+
+/**
+ * Defines step definitions that are generally useful in this project.
+ */
+class DrupalContext extends RawDrupalContext {
+}
