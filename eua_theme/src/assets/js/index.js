eed3f1df4ce57da628d7e6c995335b484e9bb2c9
Alexis Gaillard <a@bypopcorn.com>
2020-09-03 14:46:29 +0200

    G8AGE-9: Add Bootstrap 5 webpack config.

---
+++ eua_theme/src/assets/js/index.js
@@ -0,0 +1,31 @@
+// Compile scss file
+import "@scss/styles.scss";
+
+
+// Option 1
+// -----------------------------------------------------
+// Import all of Bootstrap's JS
+
+import 'bootstrap';
+
+// Option 2
+// -----------------------------------------------------
+// Import just the js you need. Note that some js are required no matter what.
+
+// import "bootstrap/js/dist/alert.js";
+// import "bootstrap/js/dist/button.js";
+// import "bootstrap/js/dist/carousel.js";
+// import "bootstrap/js/dist/collapse.js";
+// import "bootstrap/js/dist/dropdown.js";
+// import "bootstrap/js/dist/modal.js";
+// import "bootstrap/js/dist/popover.js";
+// import "bootstrap/js/dist/scrollspy.js";
+// import "bootstrap/js/dist/tab.js";
+// import "bootstrap/js/dist/toast.js";
+// import "bootstrap/js/dist/tooltip.js";
+
+
+// Custom CSS
+// -----------------------------------------------------
+
+// import "custom.js"
