f3d94f9a7f773ade0d85ba8869d82b2e76eb1654
Alexis Gaillard <a@bypopcorn.com>
2020-09-18 00:13:12 +0200

    D8AGE-18: Use laravel-mix instead of pure webpack for base theme.

---
+++ eua_theme/webpack.mix.js
@@ -0,0 +1,72 @@
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your application. See https://github.com/JeffreyWay/laravel-mix.
+ |
+ */
+const proxy = 'http://localhost';
+const mix = require('laravel-mix');
+
+if (!mix.inProduction()) {
+  // Enable source maps.
+  // See https://www.drupal.org/project/radix/issues/3021020#comment-13116504
+  mix
+    .webpackConfig({
+      devtool: 'source-map'
+    })
+    .sourceMaps();
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Configuration
+ |--------------------------------------------------------------------------
+ */
+mix
+  .setPublicPath('assets')
+  .disableNotifications()
+  .options({
+    processCssUrls: false,
+    postCss: [require("autoprefixer")]
+  })
+  .webpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          loader: 'import-glob-loader',
+        }
+      ]
+    }
+  });
+
+/*
+ |--------------------------------------------------------------------------
+ | Browsersync
+ |--------------------------------------------------------------------------
+ */
+mix.browserSync({
+  proxy: proxy,
+  files: [
+    'assets/js/**/*.js',
+    'assets/css/**/*.css',
+  ],
+  stream: true,
+});
+
+/*
+ |--------------------------------------------------------------------------
+ | SASS
+ |--------------------------------------------------------------------------
+ */
+mix.sass('src/sass/eua.style.scss', 'css');
+
+/*
+ |--------------------------------------------------------------------------
+ | JS
+ |--------------------------------------------------------------------------
+ */
+mix.js('src/js/eua.script.js', 'js');
