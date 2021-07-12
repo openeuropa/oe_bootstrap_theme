1a98ccda03649ccd5f41d879e05d82b43167378e
Alexis Gaillard <a@bypopcorn.com>
2021-07-12 11:07:21 +0200

    FRONT-2362: Refactor Sub-theme Libraries.

--- kits/default/bcl-builder.config.js
+++ kits/default/bcl-builder.config.js
@@ -1,20 +1,4 @@
 const path = require("path");
-const fs = require("fs");
-
-/* Specify base_theme relative path */
-const baseThemeCandidates = [
-  // From lib folder (DEV build).
-  "../../",
-  // From dist/web/themes/custom folder (DIST build).
-  "../../contrib/oe_bootstrap_theme"
-];
-const baseThemeFolder = baseThemeCandidates
-  .map(function (value) {
-    return path.resolve(__dirname, value);
-  })
-  .find(function (value) {
-    return fs.existsSync(value);
-  });
 
 const outputFolder = path.resolve(__dirname);
 const nodeModules = path.resolve(__dirname, "./node_modules");
@@ -23,17 +7,6 @@ const nodeModules = path.resolve(__dirname, "./node_modules");
 const includePaths = [nodeModules];
 
 module.exports = {
-  styles: [
-    {
-      entry: path.resolve(baseThemeFolder, "resources/sass/oe_bootstrap_theme.style.scss"),
-      dest: path.resolve(outputFolder, "assets/css/oe_bootstrap_theme.style.min.css"),
-      options: {
-        includePaths,
-        minify: true,
-        sourceMap: "file",
-      },
-    },
-  ],
   styles: [
     {
       entry: path.resolve(outputFolder, "resources/sass/default.style.scss"),
@@ -61,26 +34,6 @@ module.exports = {
         path.resolve(
           nodeModules,
           "@openeuropa/bcl-theme-default/js/**"
-        ),
-      ],
-      to: path.resolve(outputFolder, "assets/js"),
-      options: { up: true },
-    },
-    {
-      from: [
-        path.resolve(
-          nodeModules,
-          "bootstrap-ie11/css/**"
-        ),
-      ],
-      to: path.resolve(outputFolder, "assets/css"),
-      options: { up: true },
-    },
-    {
-      from: [
-        path.resolve(
-          baseThemeFolder,
-          "resources/js/libraries/extend/progress.js"
         ),
       ],
       to: path.resolve(outputFolder, "assets/js"),
