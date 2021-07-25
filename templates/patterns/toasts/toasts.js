69c7d3661ca9f5ba606022b6a333fd61ce39788c
Alexis Gaillard <a@bypopcorn.com>
2021-07-26 00:03:41 +0200

    FRONT-2362: Add toasts pattern.

---
+++ templates/patterns/toasts/toasts.js
@@ -0,0 +1,12 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var toastElList = [].slice.call(document.querySelectorAll(".toast"));
+    var options = { autohide: false };
+    var toastList = toastElList.map(function (toastEl) {
+    var toast = new bootstrap.Toast(toastEl, options);
+      toast.show();
+    });
+  },
+  false
+);
