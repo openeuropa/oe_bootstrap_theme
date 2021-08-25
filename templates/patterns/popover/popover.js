d61705cceeebef1a6a222d786ff57217d83442c3
escuriola <escuriola@gmail.com>
2021-08-25 11:18:23 +0200

    FRONT-2486: Popover pattern.

---
+++ templates/patterns/popover/popover.js
@@ -0,0 +1,10 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+  },
+  false
+);
