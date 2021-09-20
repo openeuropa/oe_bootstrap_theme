122a76c39d37a5302c06ed687ab2090297a85a4d
Abel Santos <abel.santos.corral@gmail.com>
2021-09-20 18:05:25 +0200

    FRONT-2486: Change popoverList at popover.js.

--- templates/patterns/popover/popover.js
+++ templates/patterns/popover/popover.js
@@ -2,7 +2,7 @@ document.addEventListener(
   "DOMContentLoaded",
   function () {
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    popoverTriggerList.map(function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
     })
   },
