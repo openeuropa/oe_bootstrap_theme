7b556f5129ae3f93b0e8eb6d4ec77ace0223f8b9
Abel Santos <abel.santos.corral@gmail.com>
2021-09-20 18:16:12 +0200

    FRONT-2486: Add drupal behaviours at popover.js.

--- templates/patterns/popover/popover.js
+++ templates/patterns/popover/popover.js
@@ -1,10 +1,9 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
+Drupal.behaviors.popover = {
+  attach: function () {
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
     popoverTriggerList.map(function (popoverTriggerEl) {
       return new bootstrap.Popover(popoverTriggerEl)
     })
-  },
-  false
-);
+  }
+
+};
