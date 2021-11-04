b89cbeeaebf1a904a18a6bfbf8d9e2741d453041
escuriola <escuriola@gmail.com>
2021-11-04 16:02:29 +0100

    OEL-493: Adapt pattern to get image.

---
+++ src/ValueObject/ValueObjectInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_bootstrap_theme\ValueObject;
+
+use Drupal\Core\Cache\RefinableCacheableDependencyInterface;
+
+/**
+ * Interface implemented by all field type value objects.
+ */
+interface ValueObjectInterface extends \ArrayAccess, \IteratorAggregate, RefinableCacheableDependencyInterface {
+
+  /**
+   * Get value object as an array.
+   *
+   * @return array
+   *   An array of property values, keyed by property name.
+   */
+  public function getArray(): array;
+
+  /**
+   * Build and return a value object from a given array.
+   *
+   * @param array $values
+   *   List of values.
+   *
+   * @return \Drupal\oe_bootstrap_theme\ValueObject\ValueObjectInterface
+   *   A new ValueObjectInterface object.
+   */
+  public static function fromArray(array $values = []): ValueObjectInterface;
+
+}
