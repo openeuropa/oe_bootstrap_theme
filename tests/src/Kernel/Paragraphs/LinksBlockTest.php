b771b3400e0a3b12cee7d975d27a1e8cfbb20c29
escuriola <escuriola@gmail.com>
2021-11-11 18:25:20 +0100

    OEL-713: Paragraph testings with all variants and backgrounds.

--- tests/src/Kernel/Paragraphs/LinksBlockTest.php
+++ tests/src/Kernel/Paragraphs/LinksBlockTest.php
@@ -22,6 +22,7 @@ class LinksBlockTest extends ParagraphsTestBase {
       'type' => 'oe_social_media_follow',
       'field_oe_title' => 'Social media title',
       'field_oe_social_media_variant' => 'horizontal',
+      'field_oe_links_block_background' => 'gray',
       'field_oe_social_media_links' => $this->getSocialMediaLinks(),
       'field_oe_social_media_see_more' => [
         'title' => 'Other social networks',
@@ -119,10 +120,106 @@ class LinksBlockTest extends ParagraphsTestBase {
     $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#youtube\']'));
     $this->assertStringContainsString('Other social networks', $links_html);
 
+    // Testing: SocialMediaFollow horizontal transparent.
+    $paragraph->get('field_oe_social_media_variant')->setValue('horizontal');
+    $paragraph->get('field_oe_links_block_background')->setValue('transparent');
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(0, $crawler->filter('div.bg-light.px-4.py-3'));
+    $this->assertCount(1, $crawler->filter('h5.fw-bold.pb-3.mb-3.border-bottom'));
+    $this->assertCount(1, $crawler->filter('ul.ps-0.mb-0'));
+    $this->assertCount(15, $crawler->filter('li.list-unstyled'));
+    $this->assertCount(14, $crawler->filter('li.list-unstyled.me-4-5'));
+    $this->assertCount(15, $crawler->filter('li.d-inline'));
+
+    // Verify that links are rendered.
+    $links = $crawler->filter('ul');
+
+    // Verify that the paragraph contains all the links.
+    $links_html = $links->html();
+    $this->assertStringContainsString('Email', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 5) = \'#email\']'));
+    $this->assertStringContainsString('Facebook', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 8) = \'#facebook\']'));
+    $this->assertStringContainsString('Flickr', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#flickr\']'));
+    $this->assertStringContainsString('Google+', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#google\']'));
+    $this->assertStringContainsString('Instagram', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 9) = \'#instagram\']'));
+    $this->assertStringContainsString('LinkedIn', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 8) = \'#linkedin\']'));
+    $this->assertStringContainsString('Pinterest', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 9) = \'#pinterest\']'));
+    $this->assertStringContainsString('RSS', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 3) = \'#rss\']'));
+    $this->assertStringContainsString('Storify', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#storify\']'));
+    $this->assertStringContainsString('1st Twitter', $links_html);
+    $this->assertStringContainsString('2nd Twitter', $links_html);
+    $this->assertCount(2, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#twitter\']'));
+    $this->assertStringContainsString('Yammer', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#yammer\']'));
+    $this->assertStringContainsString('Youtube', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#youtube\']'));
+    $this->assertStringContainsString('Other social networks', $links_html);
+
+    // Testing: SocialMediaFollow vertical transparent.
+    $paragraph->get('field_oe_social_media_variant')->setValue('vertical');
+    $paragraph->get('field_oe_links_block_background')->setValue('transparent');
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(0, $crawler->filter('div.bg-light.px-4.py-3'));
+    $this->assertCount(1, $crawler->filter('h5.fw-bold.pb-3.mb-3.border-bottom'));
+    $this->assertCount(1, $crawler->filter('ul.ps-0.mb-0'));
+    $this->assertCount(15, $crawler->filter('li.list-unstyled'));
+    $this->assertCount(14, $crawler->filter('li.list-unstyled.me-4-5'));
+    $this->assertCount(0, $crawler->filter('li.d-inline'));
+
+    // Verify that links are rendered.
+    $links = $crawler->filter('ul');
+
+    // Verify that the paragraph contains all the links.
+    $links_html = $links->html();
+    $this->assertStringContainsString('Email', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 5) = \'#email\']'));
+    $this->assertStringContainsString('Facebook', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 8) = \'#facebook\']'));
+    $this->assertStringContainsString('Flickr', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#flickr\']'));
+    $this->assertStringContainsString('Google+', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#google\']'));
+    $this->assertStringContainsString('Instagram', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 9) = \'#instagram\']'));
+    $this->assertStringContainsString('LinkedIn', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 8) = \'#linkedin\']'));
+    $this->assertStringContainsString('Pinterest', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 9) = \'#pinterest\']'));
+    $this->assertStringContainsString('RSS', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 3) = \'#rss\']'));
+    $this->assertStringContainsString('Storify', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#storify\']'));
+    $this->assertStringContainsString('1st Twitter', $links_html);
+    $this->assertStringContainsString('2nd Twitter', $links_html);
+    $this->assertCount(2, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#twitter\']'));
+    $this->assertStringContainsString('Yammer', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 6) = \'#yammer\']'));
+    $this->assertStringContainsString('Youtube', $links_html);
+    $this->assertCount(1, $crawler->filterXPath('//*[name()=\'use\' and substring(@*, string-length(@*) - 7) = \'#youtube\']'));
+    $this->assertStringContainsString('Other social networks', $links_html);
+
     // Create Links Block paragraph.
     $paragraph = Paragraph::create([
       'type' => 'oe_links_block',
       'field_oe_text' => 'More information',
+      'field_oe_links_block_variant' => 'vertical',
+      'field_oe_links_block_background' => 'gray',
       'field_oe_links' => $this->getBlockLinks(),
     ]);
     $paragraph->save();
@@ -137,6 +234,49 @@ class LinksBlockTest extends ParagraphsTestBase {
     $this->assertCount(3, $crawler->filter('li.list-unstyled'));
     $this->assertCount(2, $crawler->filter('li.list-unstyled.me-4-5'));
     $this->assertCount(0, $crawler->filter('li.d-inline'));
+
+    // Testing: LinksBlock horizontal gray.
+    $paragraph->get('field_oe_links_block_variant')->setValue('horizontal');
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(1, $crawler->filter('div.bg-light.px-4.py-3'));
+    $this->assertCount(1, $crawler->filter('h5.fw-bold.pb-3.mb-3.border-bottom'));
+    $this->assertCount(1, $crawler->filter('ul.ps-0.mb-0'));
+    $this->assertCount(3, $crawler->filter('li.list-unstyled'));
+    $this->assertCount(2, $crawler->filter('li.list-unstyled.me-4-5'));
+    $this->assertCount(3, $crawler->filter('li.d-inline'));
+
+    // Testing: LinksBlock vertical transparent.
+    $paragraph->get('field_oe_links_block_variant')->setValue('vertical');
+    $paragraph->get('field_oe_links_block_background')->setValue('transparent');
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(0, $crawler->filter('div.bg-light.px-4.py-3'));
+    $this->assertCount(1, $crawler->filter('h5.fw-bold.pb-3.mb-3.border-bottom'));
+    $this->assertCount(1, $crawler->filter('ul.ps-0.mb-0'));
+    $this->assertCount(3, $crawler->filter('li.list-unstyled'));
+    $this->assertCount(2, $crawler->filter('li.list-unstyled.me-4-5'));
+    $this->assertCount(0, $crawler->filter('li.d-inline'));
+
+    // Testing: LinksBlock horizontal transparent.
+    $paragraph->get('field_oe_links_block_variant')->setValue('horizontal');
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(0, $crawler->filter('div.bg-light.px-4.py-3'));
+    $this->assertCount(1, $crawler->filter('h5.fw-bold.pb-3.mb-3.border-bottom'));
+    $this->assertCount(1, $crawler->filter('ul.ps-0.mb-0'));
+    $this->assertCount(3, $crawler->filter('li.list-unstyled'));
+    $this->assertCount(2, $crawler->filter('li.list-unstyled.me-4-5'));
+    $this->assertCount(3, $crawler->filter('li.d-inline'));
   }
 
   /**
