4c7b47ffebe76a445b462d83986b3270d9bf0b52
escuriola <escuriola@gmail.com>
2021-11-11 10:21:16 +0100

    OEL-713: Links Block updated and social media added.

--- tests/src/Kernel/MarkupRenderingTest.php
+++ tests/src/Kernel/MarkupRenderingTest.php
@@ -62,6 +62,7 @@ class MarkupRenderingTest extends KernelTestBase implements FormInterface {
     'icon',
     'jumbotron',
     'link',
+    'link_block',
     'list',
     'list_group',
     'listing',
