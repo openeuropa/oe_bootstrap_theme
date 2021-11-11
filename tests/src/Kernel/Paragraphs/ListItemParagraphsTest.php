7721317d866debf8311bb6fe645703078b6e77ac
Abel Santos <abel.santos.corral@gmail.com>
2021-11-11 15:07:43 +0100

    OEL-490: Add date and highlight variants at test.

--- tests/src/Kernel/Paragraphs/ListItemParagraphsTest.php
+++ tests/src/Kernel/Paragraphs/ListItemParagraphsTest.php
@@ -74,6 +74,75 @@ class ListItemParagraphsTest extends ParagraphsTestBase {
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut ex tristique, dignissim sem ac, bibendum est. Sed vehicula lorem non nunc tincidunt hendrerit. Nunc tristique ante et fringilla fermentum.',
       $text_element->text()
     );
+
+    // Variant - date / Image - No / Date - Yes.
+    $paragraph->get('oe_paragraphs_variant')->setValue('date');
+    $paragraph->get('field_oe_image')->setValue([]);
+    $paragraph->get('field_oe_date')->setValue('2011-11-13');
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+    $crawler = new Crawler($html);
+
+    $this->assertCount(1, $crawler->filter('div.card'));
+    $this->assertCount(1, $crawler->filter('div.card-body'));
+    $this->assertCount(1, $crawler->filter('h5.card-title'));
+    $this->assertStringContainsString('Dot1', trim($crawler->filter('span[class="me-2 badge bg-primary"]')->text()));
+    $this->assertStringContainsString('Dot2', trim($crawler->filter('span[class="badge bg-primary"]')->text()));
+    $this->assertStringContainsString('Card Title 1', trim($crawler->filter('h5.card-title')->text()));
+    $link_element = $crawler->filter('h5.card-title a');
+    $this->assertCount(1, $link_element);
+    $this->assertStringContainsString(
+      'http://www.example.com/',
+      $link_element->attr('href')
+    );
+    $text_element = $crawler->filter('p.card-text.mb-3');
+    $this->assertCount(1, $text_element);
+    $this->assertStringContainsString(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut ex tristique, dignissim sem ac, bibendum est. Sed vehicula lorem non nunc tincidunt hendrerit. Nunc tristique ante et fringilla fermentum.',
+      $text_element->text()
+    );
+    $this->assertCount(1, $crawler->filter('span.d-md-inline.d-block.text-muted.mb-2.mb-md-0'));
+    $this->assertCount(1, $crawler->filter('time[datetime="2011-11-13T12:00:00Z"]'));
+
+    // Variant - highlight / Date - No.
+    $paragraph->get('oe_paragraphs_variant')->setValue('highlight');
+    $paragraph->get('field_oe_image')->setValue([
+      'title' => 'Example Image',
+      'alt' => 'Alt for image',
+      'target_id' => $en_file->id(),
+    ]);
+    $paragraph->get('field_oe_date')->setValue('');
+    $paragraph->save();
+    $html = $this->renderParagraph($paragraph);
+
+    $crawler = new Crawler($html);
+
+    $this->assertCount(1, $crawler->filter('div.card'));
+    $image_element = $crawler->filter('.card-img-top');
+    $this->assertCount(1, $image_element);
+    $this->assertStringContainsString(
+      file_url_transform_relative(file_create_url($en_file->getFileUri())),
+      $image_element->attr('src')
+    );
+    $this->assertCount(1, $crawler->filter('div.card-body'));
+    $this->assertCount(1, $crawler->filter('h5.card-title'));
+    $this->assertStringContainsString('Dot1', trim($crawler->filter('span[class="me-2 badge bg-primary"]')->text()));
+    $this->assertStringContainsString('Dot2', trim($crawler->filter('span[class="badge bg-primary"]')->text()));
+    $this->assertStringContainsString('Card Title 1', trim($crawler->filter('h5.card-title')->text()));
+    $link_element = $crawler->filter('h5.card-title a');
+    $this->assertCount(1, $link_element);
+    $this->assertStringContainsString(
+      'http://www.example.com/',
+      $link_element->attr('href')
+    );
+    $text_element = $crawler->filter('p.card-text.mb-2');
+    $this->assertCount(1, $text_element);
+    $this->assertStringContainsString(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut ex tristique, dignissim sem ac, bibendum est. Sed vehicula lorem non nunc tincidunt hendrerit. Nunc tristique ante et fringilla fermentum.',
+      $text_element->text()
+    );
+    $this->assertCount(0, $crawler->filter('span.d-md-inline.d-block.text-muted.mb-2.mb-md-0'));
+    $this->assertCount(0, $crawler->filter('time[datetime="2011-11-13T12:00:00Z"]'));
   }
 
 }
