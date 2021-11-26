8fe38c1dd0023d416d3cf4769d1d3165b1f78a15
Abel Santos <abel.santos.corral@gmail.com>
2021-11-26 15:05:50 +0100

    OEL-759: Add test default for Facts and Figures.

--- tests/src/Kernel/Paragraphs/FactsFiguresTest.php
+++ tests/src/Kernel/Paragraphs/FactsFiguresTest.php
@@ -82,14 +82,15 @@ class FactsFiguresTest extends ParagraphsTestBase {
     // Testing: Facts and figures - Default layout.
     $html = $this->renderParagraph($paragraph);
     $crawler = new Crawler($html);
-    print_r($html);
 
     $this->assertCount(1, $crawler->filter('div.bcl-fact-figures.bcl-fact-figures--default'));
-    $this->assertCount(4, $crawler->filter('h4.fw-bold'));
+    $this->assertCount(6, $crawler->filter('h3.text-capitalize.fw-bold'));
+    $this->assertCount(6, $crawler->filter('h5.fw-bold'));
     $this->assertCount(1, $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3'));
-    $this->assertCount(1, $crawler->filter('svg.bi.icon--l'));
+    $this->assertCount(6, $crawler->filter('svg.bi.icon--l'));
     $this->assertCount(6, $crawler->filter('h3.text-capitalize.fw-bold.mt-2'));
     $this->assertCount(6, $crawler->filter('h5.fw-bold'));
+    $this->assertCount(6, $crawler->filter('div.col.h-100'));
 
     $link = $crawler->filter('a[href="https://www.readmore.com"]');
     $this->assertStringContainsString(
@@ -97,6 +98,101 @@ class FactsFiguresTest extends ParagraphsTestBase {
       $link->html()
     );
 
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(1) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '1529 JIRA Ticket',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(1) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Jira Tickets',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(1) > p');
+    $this->assertStringContainsString(
+      'Nunc condimentum sapien ut nibh finibus suscipit vitae at justo. Morbi quis odio faucibus, commodo tortor id, elementum libero.',
+      $description_fact->html()
+    );
+
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(2) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '337 Features',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(2) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Feature tickets',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(2) > p');
+    $this->assertStringContainsString(
+      'Turpis varius congue venenatis, erat dui feugiat felis.',
+      $description_fact->html()
+    );
+
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(3) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '107 Tests',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(3) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Test tickets',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(3) > p');
+    $this->assertStringContainsString(
+      'Cras vestibulum efficitur mi, quis porta tellus rutrum ut. Quisque at pulvinar sem.',
+      $description_fact->html()
+    );
+
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(4) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '5670 Variants',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(4) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Test variants',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(4) > p');
+    $this->assertStringContainsString(
+      'Aliquam lacinia diam eu sem malesuada, in interdum ante bibendum.',
+      $description_fact->html()
+    );
+
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(5) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '345 Dev Ticket',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(5) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Jira ticket',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(5) > p');
+    $this->assertStringContainsString(
+      'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Duis nec lectus tortor.',
+      $description_fact->html()
+    );
+
+    $title_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(6) > h3.text-capitalize.fw-bold');
+    $this->assertStringContainsString(
+      '43 Components',
+      $title_fact->html()
+    );
+    $subtitle_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(6) >  h5.fw-bold');
+    $this->assertStringContainsString(
+      'Figma components',
+      $subtitle_fact->html()
+    );
+    $description_fact = $crawler->filter('div.row-cols-md-2.row.row-cols-2.g-4.mt-3 > div.col.h-100:nth-child(6) > p');
+    $this->assertStringContainsString(
+      'Sed efficitur bibendum rutrum. Nunc feugiat congue augue ac consectetur.',
+      $description_fact->html()
+    );
   }
 
 }
