445e7c2f325704613398c4bbf487ac9f84936c98
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2021-11-30 17:11:00 +0100

    OEL-494: Issues solved according to the icons.

---
+++ modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_bootstrap_theme_helper\TwigExtension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Collection of extra Twig extensions as filters.
+ *
+ * We don't enforce any strict type checking on filters' arguments as they are
+ * coming straight from Twig templates.
+ */
+class TwigExtension extends AbstractExtension {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFilters(): array {
+    return [
+      new TwigFilter('format_size', 'format_size'),
+      new TwigFilter('to_file_icon', [$this, 'toFileIcon']),
+    ];
+  }
+
+  /**
+   * Get file icon class given its extension.
+   *
+   * @param string $extension
+   *   File extension.
+   *
+   * @return string
+   *   File icon class name.
+   */
+  public function toFileIcon(string $extension): string {
+    $extension = strtolower($extension);
+    $extension_mapping = [
+      'image' => [
+        'jpg',
+        'jpeg',
+        'gif',
+        'png',
+        'webp',
+      ],
+      'presentation' => [
+        'ppt',
+        'pptx',
+        'pps',
+        'ppsx',
+        'odp',
+      ],
+      'spreadsheet' => [
+        'xls',
+        'xlsx',
+        'ods',
+      ],
+      'video' => [
+        'mp4',
+        'mov',
+        'mpeg',
+        'avi',
+        'm4v',
+        'webm',
+      ],
+      'file-text-fill' => [
+        'txt',
+      ],
+      'file-pdf-fill' => [
+        'pdf',
+      ],
+    ];
+
+    foreach ($extension_mapping as $file_type => $extensions) {
+      if (in_array($extension, $extensions)) {
+        return $file_type;
+      }
+    }
+
+    return 'file';
+  }
+
+}
