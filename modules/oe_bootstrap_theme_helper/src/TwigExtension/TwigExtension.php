13b51b9dac06e6cd21bafffeeb3eb4845acae6b1
Sergio Elvira Perez <sergioelviraperez@gmail.com>
2021-12-01 18:46:15 +0100

    OEL-494: Template modified, extra filetype added.

--- modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
+++ modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
@@ -69,6 +69,10 @@ class TwigExtension extends AbstractExtension {
       ],
       'file-pdf-fill' => [
         'pdf',
+        'application/pdf',
+      ],
+      'file-word-fill' => [
+        'word',
       ],
     ];
 
