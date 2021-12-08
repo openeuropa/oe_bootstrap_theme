9a7957170f80a83d64faf4e081234f4351e52c13
Abel Santos <abel.santos.corral@gmail.com>
2021-12-08 17:57:24 +0100

    OEL-759: Add comment into ConfigUtil.

--- src/ConfigUtil.php
+++ src/ConfigUtil.php
@@ -3,6 +3,7 @@
 namespace Drupal\oe_bootstrap_theme;
 
 use Drupal\Core\Config\FileStorage;
+use Drupal\Component\Utility\Crypt;
 
 /**
  * Utility class with static methods related to configuration stuff.
@@ -31,6 +32,11 @@ class ConfigUtil {
       $entity_storage = $entity_type_manager->getStorage($entity_type);
       $id_key = $entity_storage->getEntityType()->getKey('id');
       $entity = $entity_storage->load($configuration[$id_key]);
+      // When we create a new config, it usually means that we are also shipping
+      // it in the config/install folder, so we must ensure it gets the hash
+      // so Drupal treats it as a shipped conf. This means that it gets exposed
+      // to be translated via the locale system as well.
+      $configuration['_core']['default_config_hash'] = Crypt::hashBase64(serialize($configuration));
       $entity = $entity_storage->updateFromStorageRecord($entity, $configuration);
       $entity->save();
     }
