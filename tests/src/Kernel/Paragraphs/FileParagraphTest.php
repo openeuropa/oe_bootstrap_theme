bf6ecc924ddb44621f9623aa0cd1c70e8fbd7a43
escuriola <escuriola@gmail.com>
2021-12-09 20:05:12 +0100

    OEL-494: Fixing paragraph testing.

--- tests/src/Kernel/Paragraphs/FileParagraphTest.php
+++ tests/src/Kernel/Paragraphs/FileParagraphTest.php
@@ -4,9 +4,6 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\oe_bootstap_theme\Kernel\Patterns;
 
-use Drupal\Core\Site\Settings;
-use Drupal\file\Entity\File;
-use Drupal\oe_bootstrap_theme\ValueObject\FileValueObject;
 use Drupal\Tests\oe_bootstrap_theme\Kernel\Paragraphs\ParagraphsTestBase;
 use Symfony\Component\DomCrawler\Crawler;
 
@@ -18,138 +15,134 @@ class FileParagraphTest extends ParagraphsTestBase {
   /**
    * {@inheritdoc}
    */
-  public static $modules = [
-    'file',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
   protected function setUp(): void {
     parent::setUp();
-    $this->installEntitySchema('file');
+
+    $this->container->get('module_handler')->loadInclude('oe_paragraphs_media_field_storage', 'install');
+    oe_paragraphs_media_field_storage_install(FALSE);
+    $this->installEntitySchema('node');
+    $this->installSchema('node', ['node_access']);
+    $this->installEntitySchema('media');
+    $this->installConfig([
+      'media',
+      'oe_media',
+      'oe_paragraphs_media',
+      'media_avportal',
+      'oe_media_avportal',
+      'oe_paragraphs_banner',
+      'oe_paragraphs_document',
+      'oe_paragraphs_iframe_media',
+      'options',
+      'oe_media_iframe',
+      'oe_multilingual',
+    ]);
+
+    $this->container->get('module_handler')->loadInclude('oe_multilingual', 'install');
+    oe_multilingual_install(FALSE);
+
+    // Call the install hook of the Media module.
+    module_load_include('install', 'media');
+    media_install();
+
   }
 
   /**
    * Test file pattern rendering.
-   *
-   * @param array $file
-   *   A file array.
-   *
-   * @throws \Exception
-   *
-   * @dataProvider dataProvider
    */
-  public function testFilePatternRendering(array $file) {
-    $settings = Settings::getAll();
-    $settings['file_public_base_url'] = 'http://example.com';
-    new Settings($settings);
-    $file_created = FileValueObject::fromFileEntity(File::create($file['file']));
-    $file_created->setLanguageCode('English');
-    $file_created->meta = '(' . format_size($file_created->getSize()) .
-      ' - ' . strtoupper($file_created->getExtension()) . ')';
-
-    // Asserting languages files exist.
-    $pattern = [
-      '#type' => 'pattern',
-      '#id' => 'file',
-      '#fields' => [
-        'button_label' => 'Download',
-        'file' => $file_created,
-        'translation' => $file['translation'],
-      ],
+  public function testDocument(): void {
+    // Make document media translatable.
+    $this->container->get('content_translation.manager')->setEnabled('media', 'document', TRUE);
+    // Make fields translatable.
+    $field_ids = [
+      'media.document.oe_media_file_type',
+      'media.document.oe_media_remote_file',
+      'media.document.oe_media_file',
     ];
+    foreach ($field_ids as $field_id) {
+      $field_config = $this->container->get('entity_type.manager')->getStorage('field_config')->load($field_id);
+      $field_config->set('translatable', TRUE)->save();
+    }
+    $this->container->get('router.builder')->rebuild();
 
-    $html = $this->renderRoot($pattern);
+    $field_config = $this->container->get('entity_type.manager')->getStorage('field_config')->load('media.document.oe_media_file');
+    $field_config->set('translatable', TRUE)->save();
+    $this->container->get('router.builder')->rebuild();
+
+    // Create English file.
+    $en_image_file = file_save_data(file_get_contents(drupal_get_path('theme', 'oe_bootstrap_theme') . '/tests/fixtures/hello.txt'), 'public://hello.txt');
+    $en_image_file->setPermanent();
+    $en_image_file->save();
+
+    $media_storage = $this->container->get('entity_type.manager')->getStorage('media');
+    /** @var \Drupal\media\MediaInterface $media */
+    $media = $media_storage->create([
+      'bundle' => 'document',
+      'name' => 'druplicon.txt',
+      'oe_media_file_type' => 'local',
+      'oe_media_file' => [
+        'target_id' => (int) $en_image_file->id(),
+      ],
+    ]);
+    $media->save();
+
+    // Create Other language files.
+    $other_languages = ['fr', 'it', 'es'];
+
+    foreach ($other_languages as $lang) {
+      $image_file[$lang] = file_save_data(file_get_contents(drupal_get_path('theme', 'oe_bootstrap_theme') . '/tests/fixtures/hello.txt'), "public://example_$lang.txt");
+      $image_file[$lang]->setPermanent();
+      $image_file[$lang]->save();
+
+      $media->addTranslation($lang, [
+        'name' => 'test document ' . $lang,
+        'oe_media_file_type' => 'local',
+        'oe_media_file' => [
+          'target_id' => (int) $image_file[$lang]->id(),
+        ],
+      ]);
+      $media->save();
+    }
+
+    $paragraph_storage = $this->container->get('entity_type.manager')->getStorage('paragraph');
+    $paragraph = $paragraph_storage->create([
+      'type' => 'oe_document',
+      'field_oe_media' => [
+        'target_id' => $media->id(),
+      ],
+    ]);
+    $paragraph->save();
+
+    $html = $this->renderParagraph($paragraph);
     $crawler = new Crawler($html);
 
     $this->assertCount(3, $crawler->filter('div.row'));
     $this->assertCount(4, $crawler->filter('small.fw-bold.m-0'));
-    $this->assertCount(1, $crawler->filter('.collapse .py-3.border-bottom:nth-child(1) a[href="/example1.html"]'));
-    $this->assertCount(1, $crawler->filter('.collapse .py-3.border-bottom:nth-child(2) a[href="/example2.html"]'));
-    $this->assertCount(1, $crawler->filter('.collapse .pt-3 a[href="/example3.html"]'));
+    $this->assertCount(1, $crawler->filter('.collapse .py-3.border-bottom:nth-child(1) a'));
+    $this->assertCount(1, $crawler->filter('.collapse .py-3.border-bottom:nth-child(2) a'));
+    $this->assertCount(1, $crawler->filter('.collapse .pt-3 a'));
 
     $this->assertEquals('druplicon.txt', $crawler->filter('p.fw-bold.m-0')->text());
     $this->assertEquals('Other languages (3)', $crawler->filter('div.text-end a.text-underline-hover')->text());
-    $this->assertEquals('English (23.99 KB - TXT)', $crawler->filter('small.fw-bold.m-0')->text());
-    $this->assertEquals('български (16.3 MB - PDF)', $crawler->filter('.py-3.border-bottom:nth-child(1) small')->text());
-    $this->assertEquals('Español (18.4 MB - PDF)', $crawler->filter('.py-3.border-bottom:nth-child(2) small')->text());
-    $this->assertEquals('Français (16.8 MB - PDF)', $crawler->filter('.pt-3 small')->text());
+    $this->assertEquals('English (12 bytes - TXT)', $crawler->filter('small.fw-bold.m-0')->text());
+    $this->assertEquals('Spanish (12 bytes - TXT)', $crawler->filter('.py-3.border-bottom:nth-child(1) small')->text());
+    $this->assertEquals('French (12 bytes - TXT)', $crawler->filter('.py-3.border-bottom:nth-child(2) small')->text());
+    $this->assertEquals('Italian (12 bytes - TXT)', $crawler->filter('.pt-3 small')->text());
 
-    // Asserting languages files doesn't exist.
-    $pattern = [
-      '#type' => 'pattern',
-      '#id' => 'file',
-      '#fields' => [
-        'button_label' => 'Download',
-        'file' => $file_created,
-      ],
-    ];
-    $html = $this->renderRoot($pattern);
+    foreach ($other_languages as $lang) {
+      $media->removeTranslation($lang);
+      $media->save();
+    }
+
+    $html = $this->renderParagraph($paragraph);
     $crawler = new Crawler($html);
+
     $this->assertCount(0, $crawler->filter('div.row'));
     $this->assertCount(1, $crawler->filter('small.fw-bold.m-0'));
 
     $this->assertEquals('druplicon.txt', $crawler->filter('p.fw-bold.m-0')->text());
-    $this->assertEquals('English (23.99 KB - TXT)', $crawler->filter('small.fw-bold.m-0')->text());
-    $this->assertEquals('http://example.com/sample/druplicon.txt', $crawler->filter('.text-underline-hover')->attr('href'));
-    $this->assertEquals('/assets/icons/bootstrap-icons.svg#file-text-fill', $crawler->filter('.text-secondary use')->attr('xlink:href'));
-  }
-
-  /**
-   * Data provider for testFilePatternRendering.
-   *
-   * @return array
-   *   An array of test data arrays with assertions.
-   */
-  public function dataProvider(): array {
-    $data = [];
-    $data['file'] = [
-      'fields' => [
-        'file' => [
-          'uid' => '1',
-          'filename' => 'druplicon.txt',
-          'title' => 'druplicon.txt',
-          'filemime' => 'text/plain',
-          'uri' => 'public://sample/druplicon.txt',
-          'filesize' => '24564',
-          'languageCode' => 'English',
-        ],
-        'translation' => [
-          'label' => [
-            'label' => "Other languages (3)",
-          ],
-          'items' => [
-            [
-              'language' => 'български',
-              'meta' => '(16.3 MB - PDF)',
-              'download' => [
-                'label' => 'Download',
-                'path' => '/example1.html',
-              ],
-            ],
-            [
-              'language' => 'Español',
-              'meta' => '(18.4 MB - PDF)',
-              'download' => [
-                'label' => 'Download',
-                'path' => '/example2.html',
-              ],
-            ],
-            [
-              'language' => 'Français',
-              'meta' => '(16.8 MB - PDF)',
-              'download' => [
-                'label' => 'Download',
-                'path' => '/example3.html',
-              ],
-            ],
-          ],
-        ],
-      ],
-    ];
-
-    return $data;
+    $this->assertEquals('English (12 bytes - TXT)', $crawler->filter('small.fw-bold.m-0')->text());
+    $this->assertStringContainsString('hello.txt', $crawler->filter('.text-underline-hover')->attr('href'));
+    $this->assertStringContainsString('/assets/icons/bootstrap-icons.svg#file-text-fill', $crawler->filter('.text-secondary use')->attr('xlink:href'));
   }
 
 }
