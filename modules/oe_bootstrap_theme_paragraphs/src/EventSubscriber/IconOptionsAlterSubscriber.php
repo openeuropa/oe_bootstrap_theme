6836195d784f0a5b2b3217dc362a2148dfbffce7
Abel Santos <abel.santos.corral@gmail.com>
2021-12-15 09:03:11 +0100

    OEL-759: Add subscriber for event that will add icon options to field_oe_icon.

---
+++ modules/oe_bootstrap_theme_paragraphs/src/EventSubscriber/IconOptionsAlterSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_bootstrap_theme_paragraphs\EventSubscriber;
+
+use Drupal\oe_paragraphs\Event\IconOptionsEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Provides options for the icon field.
+ */
+class IconOptionsAlterSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      IconOptionsEvent::class => ['getIconOptions', 300],
+    ];
+  }
+
+  /**
+   * Gets the icon options.
+   *
+   * @param \Drupal\custom_events\Event\IconOptionsEvent $event
+   *   Allowed format event object.
+   */
+  public function getIconOptions(IconOptionsEvent $event): void {
+    $event->setIconOptions([
+      'arrow-down' => 'Arrow down',
+      'box-arrow-up' => 'External',
+      'arrow-up' => 'Arrow up',
+      'book' => 'Book',
+      'camera' => 'Camera',
+      'check' => 'Check',
+      'download' => 'Download',
+      'currency-euro' => 'Euro',
+      'facebook' => 'Facebook',
+      'file' => 'File',
+      'image' => 'Image',
+      'info' => 'Info',
+      'linkedin' => 'LinkedIn',
+      'files' => 'Multiple files',
+      'rss' => 'RSS',
+      'search' => 'Search',
+      'share' => 'Share',
+      'twitter' => 'Twitter',
+      'person-video' => 'Video',
+    ]);
+  }
+
+}
