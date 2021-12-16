53a98326bd2aecce0404e4ea00231c28e7656624
Abel Santos <abel.santos.corral@gmail.com>
2021-12-16 16:54:24 +0100

    OEL-856: Start preparing post-update to fix labels of Featured media paragraph.

---
+++ modules/oe_bootstrap_theme_paragraphs/oe_bootstrap_theme_paragraphs.post_update.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for the OE Bootstrap theme paragraphs module.
+ */
+
+declare(strict_types = 1);
+
+use Drupal\field\Entity\FieldConfig;
+
+/**
+ * Change label of fields of Text feature media paragraph.
+ *
+ * @param array $sandbox
+ *   Stores information for batch updates.
+ */
+function oe_bootstrap_theme_paragraphs_post_update_10001(array &$sandbox) {
+  $elements = [
+    [
+      'config_name' => 'field.field.paragraph.oe_text_feature_media.field_oe_title ',
+      'field_name' => 'field_oe_title',
+      'paragraph_name' => 'Oe text feature media',
+      'label' => 'Title',
+    ],
+    [
+      'config_name' => 'field.field.paragraph.oe_text_feature_media.field_oe_feature_media_title',
+      'field_name' => 'field_oe_feature_media_title',
+      'paragraph_name' => 'Oe text feature media',
+      'label' => 'Media title',
+    ],
+  ];
+
+  foreach ($elements as $element) {
+    $field = FieldConfig::load($element['config_name']);
+
+    if (!$field) {
+      sprintf('Could not load the ' . $element['field_name'] . ' in ' . $element['paragraph_name'] . ' paragraph.');
+    }
+
+    $field->setSetting('label', $element['label']);
+    $field->save();
+  }
+
+}
