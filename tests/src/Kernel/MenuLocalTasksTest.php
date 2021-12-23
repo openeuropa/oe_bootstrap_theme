913feb06a5fa2ec5defd656b348857ad8c7f4eff
escuriola <escuriola@gmail.com>
2021-12-23 12:22:12 +0100

    OEL-788: Testing.

---
+++ tests/src/Kernel/MenuLocalTasksTest.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_bootstrap_theme\Kernel;
+
+use Drupal\Core\Url;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Tests that Drupal local tasks are properly rendered.
+ */
+class MenuLocalTasksTest extends AbstractKernelTestBase {
+
+  /**
+   * Test menu local tasks.
+   */
+  public function testMenuLocalTasks(): void {
+    $render = [
+      '#theme' => 'menu_local_tasks',
+      '#primary' => [
+        'link1.link' => [
+          '#theme' => 'menu_local_task',
+          '#link' => [
+            'title' => 'Third link - Active',
+            'url' => Url::fromUri('http://www.active.com'),
+          ],
+          '#active' => TRUE,
+          '#weight' => 10,
+        ],
+        'link2.link' => [
+          '#theme' => 'menu_local_task',
+          '#link' => [
+            'title' => 'First link - Inactive',
+            'url' => Url::fromUri('http://www.inactive.com'),
+          ],
+          '#active' => FALSE,
+          '#weight' => -10,
+        ],
+        'link3.link' => [
+          '#theme' => 'menu_local_task',
+          '#link' => [
+            'title' => 'Second link',
+            'url' => Url::fromUri('http://www.middlelink.com'),
+          ],
+          '#active' => FALSE,
+          '#weight' => 0,
+        ],
+      ],
+    ];
+
+    $html = $this->renderRoot($render);
+    $crawler = new Crawler($html);
+
+    $actual = $crawler->filter('nav.nav-tabs');
+    $this->assertCount(1, $actual);
+
+    $links = $crawler->filter('a.nav-link.text-underline-hover');
+    $this->assertCount(3, $links);
+
+    $active = $crawler->filter('a.active');
+    $this->assertCount(1, $active);
+    $this->assertEquals('Third link - Active', trim($active->text()));
+
+    // Assert regular link are ordered by weight.
+    $actual = $crawler->filter('a:nth-child(1)')->text();
+    $this->assertEquals('First link - Inactive', trim($actual));
+
+    $actual = $crawler->filter('a:nth-child(2)')->text();
+    $this->assertEquals('Second link', trim($actual));
+
+    $actual = $crawler->filter('a:nth-child(3)')->text();
+    $this->assertEquals('Third link - Active', trim($actual));
+  }
+
+}
