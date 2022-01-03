d182b4a366fe3680aad58d30da6909fc09aeefa0
Abel Santos <abel.santos.corral@gmail.com>
2022-01-03 09:17:25 +0100

    OEL-753: Add a todo comment on the javascript file.

--- resources/js/libraries/extend/inpage_navigation.js
+++ resources/js/libraries/extend/inpage_navigation.js
@@ -2,6 +2,7 @@
   Drupal.behaviors.inpage_navigation = {
     attach: function attach(context, settings) {
       new bs.ScrollSpy(document.body, {
+        // TODO: At ticket OEL-848 this will be fixed to target on class and not on id.
         target: '#bcl-inpage-navigation'
       });
     }
