6dc63075201bd5575b633adec8e64b07159377fe
Abel Santos <abel.santos.corral@gmail.com>
2022-01-06 12:01:31 +0100

    OEL-856: Save work. Using post-updates and installs.

--- modules/oe_bootstrap_theme_paragraphs/oe_bootstrap_theme_paragraphs.post_update.php
+++ modules/oe_bootstrap_theme_paragraphs/oe_bootstrap_theme_paragraphs.post_update.php
@@ -7,36 +7,18 @@
 
 declare(strict_types = 1);
 
-use Drupal\field\Entity\FieldConfig;
+use Drupal\oe_bootstrap_theme\ConfigUtil;
 
 /**
- * Change label of fields of Text feature media paragraph.
+ * Set new label for fields of Featured media paragraph.
  */
 function oe_bootstrap_theme_paragraphs_post_update_10001(array &$sandbox): void {
-  $elements = [
-    [
-      'config_name' => 'field.field.paragraph.oe_text_feature_media.field_oe_title ',
-      'field_name' => 'field_oe_title',
-      'paragraph_name' => 'Oe text feature media',
-      'label' => 'Title',
-    ],
-    [
-      'config_name' => 'field.field.paragraph.oe_text_feature_media.field_oe_feature_media_title',
-      'field_name' => 'field_oe_feature_media_title',
-      'paragraph_name' => 'Oe text feature media',
-      'label' => 'Media title',
-    ],
+
+  $config['fields'] = [
+    'field.field.paragraph.oe_text_feature_media.field_oe_feature_media_title',
+    'field.field.paragraph.oe_text_feature_media.field_oe_title',
   ];
 
-  foreach ($elements as $element) {
-    $field = FieldConfig::load($element['config_name']);
-
-    if (!$field) {
-      sprintf('Could not load the ' . $element['field_name'] . ' in ' . $element['paragraph_name'] . ' paragraph.');
-    }
-
-    $field->setSetting('label', $element['label']);
-    $field->save();
-  }
+  ConfigUtil::importConfigFromFile('oe_bootstrap_theme_paragraphs', '/config/post_updates/10001/', $config);
 
 }
