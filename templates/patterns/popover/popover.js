ccfa390d2cedf5a7c15c9837724a439592e4b702
escuriola <escuriola@gmail.com>
2022-01-11 17:56:37 +0100

    OEL-779: Removing unused patterns.

--- templates/patterns/popover/popover.js
+++
@@ -1,9 +0,0 @@
-Drupal.behaviors.popover = {
-  attach: function () {
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-    popoverTriggerList.map(function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
-    })
-  }
-
-};
