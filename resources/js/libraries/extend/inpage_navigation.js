002225b98113452624fe41ae1e0d1a17bd4729ef
Abel Santos <abel.santos.corral@gmail.com>
2022-01-20 18:05:54 +0100

    OEL-1049: Eliminate comment from inpage_navigation.js.

--- resources/js/libraries/extend/inpage_navigation.js
+++ resources/js/libraries/extend/inpage_navigation.js
@@ -2,9 +2,9 @@
   Drupal.behaviors.inpage_navigation = {
     attach: function attach(context, settings) {
       new bs.ScrollSpy(document.body, {
-        // TODO: At ticket OEL-848 this will be fixed to target on class and not on id.
         target: '#bcl-inpage-navigation'
       });
     }
   };
 })(this.bootstrap, Drupal);
+
