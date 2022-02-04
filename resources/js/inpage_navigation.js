ab3bfccdea2e41d9171d4e254dcd128e10a9e6e8
Francesco Sardara <francesco.sardara@ausy.be>
2022-02-04 01:26:55 +0100

    OEL-1049: Target only the inpage navigation menus to instantiate the scrollSpy.

--- resources/js/inpage_navigation.js
+++ resources/js/inpage_navigation.js
@@ -1,10 +1,28 @@
-(function (bs, Drupal) {
+/**
+ * @file
+ * Attaches behaviors for inpage navigation pattern.
+ */
+(function (bootstrap, Drupal) {
+
+  var instances = [];
+
+  /**
+   * Attaches the scrollspy behaviours to inpage navigation elements.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Initialises a scrollSpy instance for each navigation.
+   */
   Drupal.behaviors.inpage_navigation = {
-    attach: function attach(context, settings) {
-      new bs.ScrollSpy(document.body, {
-        target: '.bcl-inpage-navigation'
+    attach: function () {
+      Array.prototype.forEach.call(document.querySelectorAll('nav.bcl-inpage-navigation'), function (nav) {
+        instances.push(new bootstrap.ScrollSpy(document.body, {
+          target: nav
+        }));
       });
     }
   };
-})(this.bootstrap, Drupal);
+
+})(bootstrap, Drupal);
 
