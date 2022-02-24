a5ceb3ee9d193418ba1b64f1fc73f68d7eeeb444
escuriola <escuriola@gmail.com>
2022-02-24 17:44:12 +0100

    OEL-755: Remove copy&pasta twig functions.

--- modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
+++ modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
@@ -37,13 +37,13 @@ class TwigExtension extends AbstractExtension {
   }
 
   /**
-   * Filters a list of cards.
+   * Maps cards items.
    *
    * @param array $items
-   *   Datetime to be parsed.
+   *   Lists of items.
    *
    * @return array
-   *   The translated time ago string.
+   *   Items mapped as a card.
    */
   public function bclCardList(array $items): array {
     $cardItems = [];
@@ -83,77 +83,6 @@ class TwigExtension extends AbstractExtension {
     }
 
     return $cardItems;
-  }
-
-  /**
-   * Processes footer links to make them compatible with BCL formatting.
-   *
-   * @param array $context
-   *   The twig context.
-   * @param array $links
-   *   Set of links to be processed.
-   *
-   * @return array
-   *   Set of processed links.
-   */
-  public function bclFooterLinks(array $context, array $links): array {
-    $altered_links = [];
-
-    foreach ($links as $link) {
-      $altered_link = [
-        'label' => $link['label'],
-        'path' => $link['href'],
-        'icon_position' => 'after',
-        'standalone' => TRUE,
-        'attributes' => [
-          'class' => [
-            'd-block',
-            'mb-1',
-          ],
-        ],
-      ];
-
-      if (!empty($link['external']) && $link['external'] === TRUE) {
-        $altered_link['icon'] = [
-          'path' => $context['bcl_icon_path'],
-          'name' => 'box-arrow-up-right',
-          'size' => 'xs',
-        ];
-      }
-
-      if (!empty($link['social_network'])) {
-        $altered_link['icon_position'] = 'before';
-        $altered_link['icon'] = [
-          'path' => $context['bcl_icon_path'],
-          'name' => $link['social_network'],
-          'size' => 'xs',
-        ];
-      }
-
-      $altered_links[] = $altered_link;
-    }
-
-    return $altered_links;
-  }
-
-  /**
-   * Builds the render array for a block.
-   *
-   * @param string $id
-   *   The block plugin ID.
-   * @param array $configuration
-   *   The block configuration.
-   *
-   * @return array
-   *   The block render array.
-   */
-  public function bclBlock(string $id, array $configuration = []): array {
-    $configuration += ['label_display' => 'hidden'];
-
-    /** @var \Drupal\Core\Block\BlockPluginInterface $block_plugin */
-    $block_plugin = $this->pluginManagerBlock->createInstance($id, $configuration);
-
-    return $block_plugin->build();
   }
 
 }
