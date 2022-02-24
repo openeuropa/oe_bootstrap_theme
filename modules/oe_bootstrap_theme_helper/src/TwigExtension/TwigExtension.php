6073101a8f506a7d7e3687ff9c53a32133661aee
escuriola <escuriola@gmail.com>
2022-02-24 16:39:19 +0100

    OEL-755: Create 'bcl_card_list' twig filter.

---
+++ modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_bootstrap_theme_helper\TwigExtension;
+
+use Drupal\Core\Cache\CacheableDependencyInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+/**
+ * Collection of extra Twig extensions as filters and functions.
+ */
+class TwigExtension extends AbstractExtension {
+
+  /**
+   * The plugin.manager.block service.
+   *
+   * @var \Drupal\Core\Cache\CacheableDependencyInterface
+   */
+  protected $pluginManagerBlock;
+
+  /**
+   * Constructs the TwigExtension object.
+   */
+  public function __construct(CacheableDependencyInterface $plugin_manager_block) {
+    $this->pluginManagerBlock = $plugin_manager_block;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFilters(): array {
+    return [
+      new TwigFilter('bcl_card_list', [$this, 'bclCardList']),
+    ];
+  }
+
+  /**
+   * Twig filter callback for 'bcl_card_list'.
+   *
+   * @param array $items
+   *   Lists of items, as sent to e.g. 'card_layout' pattern.
+   *
+   * @return array
+   *   Converted items, where each item is suitable for bcl-card.html.twig.
+   */
+  public function bclCardList(array $items): array {
+    $bcl_cards = [];
+    foreach ($items as $item) {
+      // Copy most of the fields.
+      $bcl_card = $item;
+      // Some fields need to be rewritten.
+      if (isset($item['title'])) {
+        $bcl_card['title'] = [
+          'content' => $item['title'],
+        ];
+      }
+      if (isset($item['subtitle'])) {
+        $bcl_card['subtitle'] = [
+          'content' => $item['subtitle'],
+        ];
+      }
+      if (isset($item['text'])) {
+        $bcl_card['text'] = [
+          'content' => $item['text'],
+          'classes' => 'mb-2',
+          'tag' => 'div',
+        ];
+      }
+      $bcl_card['badges'] = [];
+      foreach ($item['badges'] ?? [] as $badge) {
+        $bcl_card['badges'][] = [
+          'label' => $badge,
+          'background' => 'primary',
+        ];
+      }
+      $bcl_cards[] = $bcl_card;
+    }
+
+    return $bcl_cards;
+  }
+
+}
