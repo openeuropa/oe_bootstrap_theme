a2d0a4f2d973d0adc096c944e195875c0167967a
Abel Santos <abel.santos.corral@gmail.com>
2022-03-15 11:12:21 +0100

    OEL-1280: Add post_update at helper to uninstall oe_bootstrap_theme_paragraphs.

---
+++ modules/oe_bootstrap_theme_helper/oe_bootstrap_theme_helper.post_update.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * Post update functions for the OE Bootstrap theme helper module.
+ */
+
+declare(strict_types = 1);
+
+/**
+ * Uninstall OE Bootstrap Theme Paragraphs module.
+ */
+function oe_bootstrap_theme_paragraphs_post_update_00001(array &$sandbox): void {
+  $moduleHandler = \Drupal::service('module_handler');
+  if ($moduleHandler->moduleExists('oe_bootstrap_theme_paragraphs')) {
+    \Drupal::service('module_installer')->uninstall(['oe_bootstrap_theme_paragraphs']);
+  }
+}
