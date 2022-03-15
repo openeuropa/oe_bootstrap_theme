838f996ded49eed21d96be106501038f2e82801a
Abel Santos <abel.santos.corral@gmail.com>
2022-03-15 16:38:18 +0100

    OEL-1280: Change name of function post_update.

--- modules/oe_bootstrap_theme_helper/oe_bootstrap_theme_helper.post_update.php
+++ modules/oe_bootstrap_theme_helper/oe_bootstrap_theme_helper.post_update.php
@@ -10,7 +10,7 @@ declare(strict_types = 1);
 /**
  * Uninstall OE Bootstrap Theme Paragraphs module.
  */
-function oe_bootstrap_theme_paragraphs_post_update_00001(array &$sandbox): void {
+function oe_bootstrap_theme_helper_post_update_00001(array &$sandbox): void {
   $moduleHandler = \Drupal::service('module_handler');
   if ($moduleHandler->moduleExists('oe_bootstrap_theme_paragraphs')) {
     \Drupal::service('module_installer')->uninstall(['oe_bootstrap_theme_paragraphs']);
