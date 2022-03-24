7cdb11037fc3ef0891ce109bb7b2ae85df65d890
Francesco Sardara <francesco@tdgwebservices.com>
2022-03-24 15:37:21 +0100

    OEL-494: Add file icon and language twig filters.

--- modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
+++ modules/oe_bootstrap_theme_helper/src/TwigExtension/TwigExtension.php
@@ -4,8 +4,10 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_bootstrap_theme_helper\TwigExtension;
 
-use Drupal\Core\Cache\CacheableDependencyInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\oe_bootstrap_theme_helper\EuropeanUnionLanguages;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -15,17 +17,30 @@ use Twig\TwigFilter;
 class TwigExtension extends AbstractExtension {
 
   /**
-   * The plugin.manager.block service.
+   * The language manager.
    *
-   * @var \Drupal\Core\Cache\CacheableDependencyInterface
+   * @var \Drupal\Core\Language\LanguageManagerInterface
    */
-  protected $pluginManagerBlock;
+  protected $languageManager;
 
   /**
-   * Constructs the TwigExtension object.
+   * The renderer.
+   *
+   * @var \Drupal\Core\Render\RendererInterface
    */
-  public function __construct(CacheableDependencyInterface $plugin_manager_block) {
-    $this->pluginManagerBlock = $plugin_manager_block;
+  protected $renderer;
+
+  /**
+   * Constructs a new TwigExtension object.
+   *
+   * @param \Drupal\Core\Language\LanguageManagerInterface $languageManager
+   *   The language manager.
+   * @param \Drupal\Core\Render\RendererInterface $renderer
+   *   The renderer service.
+   */
+  public function __construct(LanguageManagerInterface $languageManager, RendererInterface $renderer) {
+    $this->languageManager = $languageManager;
+    $this->renderer = $renderer;
   }
 
   /**
@@ -34,6 +49,16 @@ class TwigExtension extends AbstractExtension {
   public function getFilters(): array {
     return [
       new TwigFilter('bcl_card_list', [$this, 'bclCardList']),
+      new TwigFilter('format_size', 'format_size'),
+      new TwigFilter('to_file_icon', [$this, 'toFileIcon']),
+      new TwigFilter('to_native_language', [
+        $this,
+        'toNativeLanguageName',
+      ]),
+      new TwigFilter('to_internal_language_id', [
+        $this,
+        'toInternalLanguageId',
+      ]),
     ];
   }
 
@@ -99,6 +124,110 @@ class TwigExtension extends AbstractExtension {
     }
 
     return $bcl_cards;
+  }
+
+  /**
+   * Get file icon class given its extension.
+   *
+   * @param string $extension
+   *   File extension.
+   *
+   * @return string
+   *   File icon class name.
+   */
+  public function toFileIcon(string $extension): string {
+    $extension = strtolower($extension);
+    $extension_mapping = [
+      'image' => [
+        'jpg',
+        'jpeg',
+        'gif',
+        'png',
+        'webp',
+      ],
+      'presentation' => [
+        'ppt',
+        'pptx',
+        'pps',
+        'ppsx',
+        'odp',
+      ],
+      'file-excel-fill' => [
+        'xls',
+        'xlsx',
+        'ods',
+      ],
+      'video' => [
+        'mp4',
+        'mp4',
+        'mpeg',
+        'avi',
+        'm4v',
+        'webm',
+      ],
+      'file-text-fill' => [
+        'txt',
+      ],
+      'file-pdf-fill' => [
+        'pdf',
+        'application/pdf',
+      ],
+      'file-word-fill' => [
+        'word',
+        'odt',
+      ],
+    ];
+
+    foreach ($extension_mapping as $icon => $extensions) {
+      if (in_array($extension, $extensions)) {
+        return $icon;
+      }
+    }
+
+    return 'file-earmark';
+  }
+
+  /**
+   * Get a native language name given its code.
+   *
+   * @param string $language_code
+   *   The language code as defined by the W3C language tags document.
+   *
+   * @return string
+   *   The native language name.
+   *
+   * @throws \InvalidArgumentException
+   *   Thrown when the passed in language code does not exist.
+   */
+  public function toNativeLanguageName($language_code): string {
+    $languages = $this->languageManager->getNativeLanguages();
+    if (!empty($languages[$language_code])) {
+      return $languages[$language_code]->getName();
+    }
+    // The fallback implemented in case we don't have enabled language.
+    $predefined = EuropeanUnionLanguages::getLanguageList() + $this->languageManager::getStandardLanguageList();
+    if (!empty($predefined[$language_code][1])) {
+      return $predefined[$language_code][1];
+    }
+
+    throw new \InvalidArgumentException('The language code ' . $language_code . ' does not exist.');
+  }
+
+  /**
+   * Get an internal language ID given its code.
+   *
+   * @param string $language_code
+   *   The language code as defined by the W3C language tags document.
+   *
+   * @return string
+   *   The internal language ID, or the given language code if none found.
+   */
+  public function toInternalLanguageId(string $language_code): string {
+    if (EuropeanUnionLanguages::hasLanguage($language_code)) {
+      return EuropeanUnionLanguages::getInternalLanguageCode($language_code);
+    }
+
+    return $language_code;
   }
 
 }
