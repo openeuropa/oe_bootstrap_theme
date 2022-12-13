0423872c1ed52c3e2ce2c731258bee246c1d3f48
Francesco Sardara <francesco@tdgwebservices.com>
2022-12-13 17:58:37 +0100

    OEL-2009: Add a filter that enable Bootstrap styling for tables.

---
+++ modules/oe_bootstrap_theme_helper/src/Plugin/Filter/BootstrapTable.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\oe_bootstrap_theme_helper\Plugin\Filter;
+
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\filter\FilterProcessResult;
+use Drupal\filter\Plugin\FilterBase;
+
+/**
+ * Provides a filter to add Bootstrap styling to tables.
+ *
+ * @Filter(
+ *   id = "oe_bootstrap_theme_table",
+ *   title = @Translation("Bootstrap tables"),
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_IRREVERSIBLE,
+ *   settings = {
+ *     "responsive" = "",
+ *   }
+ * )
+ */
+final class BootstrapTable extends FilterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['responsive'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Responsive'),
+      '#description' => $this->t('Makes the table responsive starting from the specified breakpoint. Choose "always" or "never" to turn on or off for all breakpoints.'),
+      '#options' => [
+        'always' => $this->t('Always'),
+        'sm' => $this->t('Small'),
+        'md' => $this->t('Medium'),
+        'lg' => $this->t('Large'),
+        'xl' => $this->t('Extra large'),
+        'xxl' => $this->t('Extra extra large'),
+      ],
+      '#empty_option' => $this->t('Never'),
+      '#default_value' => $this->settings['responsive'],
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function process($text, $langcode) {
+    $result = new FilterProcessResult($text);
+
+    // Ensure that we have tables in the markup.
+    if (stristr($text, '<table') === FALSE) {
+      return $result;
+    }
+
+    $dom = Html::load($text);
+    $xpath = new \DOMXPath($dom);
+
+    // Prepare the class to use if responsive is enabled.
+    $responsive = $this->settings['responsive'] !== '';
+    if ($responsive) {
+      $responsive_class = 'table-responsive';
+      if ($this->settings['responsive'] !== 'always') {
+        $responsive_class .= '-' . $this->settings['responsive'];
+      }
+    }
+
+    foreach ($xpath->query('//table') as $table) {
+      /** @var \DOMNode $table */
+      $this->nodeAddClass($table, 'table');
+
+      if (!$responsive) {
+        continue;
+      }
+
+      // Do not add the responsive wrapper to nested tables.
+      $parent_tables = $xpath->query('./ancestor::table', $table);
+      if ($parent_tables->count()) {
+        continue;
+      }
+
+      $wrapper = $dom->createElement('div');
+      $this->nodeAddClass($wrapper, $responsive_class);
+      $table->parentNode->replaceChild($wrapper, $table);
+      $wrapper->appendChild($table);
+    }
+
+    $result->setProcessedText(Html::serialize($dom));
+
+    return $result;
+  }
+
+  /**
+   * Adds a class to a DOM node.
+   *
+   * @param \DOMNode $node
+   *   Element.
+   * @param string $class
+   *   Class that should be added.
+   */
+  public function nodeAddClass(\DOMNode $node, string $class): void {
+    $classes = array_filter(array_map('trim', explode(' ', $node->getAttribute('class'))));
+
+    if (in_array($class, $classes)) {
+      return;
+    }
+
+    $classes[] = $class;
+    $node->setAttribute('class', implode(' ', $classes));
+  }
+
+}
---
+++ modules/oe_bootstrap_theme_helper/tests/src/Unit/BootstrapTableFilterTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_bootstrap_theme_helper\Unit;
+
+use Drupal\oe_bootstrap_theme_helper\Plugin\Filter\BootstrapTable;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests the bootstrap table filter.
+ */
+class BootstrapTableFilterTest extends UnitTestCase {
+
+  /**
+   * Tests the process method.
+   *
+   * @param array $settings
+   *   The plugin settings.
+   * @param string $html
+   *   The filter input.
+   * @param string $expected
+   *   The expected filter output.
+   *
+   * @dataProvider processDataProvider
+   */
+  public function testProcess(array $settings, string $html, string $expected): void {
+    $filter = new BootstrapTable(['settings' => $settings], 'oe_bootstrap_theme_table', ['provider' => 'test']);
+    $filter->setStringTranslation($this->getStringTranslationStub());
+
+    $processed_text = $filter->process($html, NULL)->getProcessedText();
+    $this->assertEquals($expected, $processed_text);
+  }
+
+  /**
+   * Data provider for testProcess().
+   *
+   * @return array[]
+   *   A list of test scenarios.
+   */
+  public function processDataProvider(): array {
+    return [
+      'No tables' => [
+        ['responsive' => ''],
+        '<p>A sample text</p>',
+        '<p>A sample text</p>',
+      ],
+      'Table with no classes' => [
+        ['responsive' => ''],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+      ],
+      'Table with existing classes' => [
+        ['responsive' => ''],
+        '<table class=" table-primary "><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<table class="table-primary table"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+      ],
+      'Table with existing table class' => [
+        ['responsive' => ''],
+        '<table class=" table  table-primary"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<table class=" table  table-primary"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+      ],
+      'Table with parameters' => [
+        ['responsive' => ''],
+        '<table id="test"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<table id="test" class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+      ],
+      'Nested table' => [
+        ['responsive' => ''],
+        '<table><tr><td>Lorem</td><td><table><tr><td>Nested</td><td>Table</td></tr></table></td></tr></table>',
+        '<table class="table"><tr><td>Lorem</td><td><table class="table"><tr><td>Nested</td><td>Table</td></tr></table></td></tr></table>',
+      ],
+      'Responsive - always' => [
+        ['responsive' => 'always'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - small' => [
+        ['responsive' => 'sm'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive-sm"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - medium' => [
+        ['responsive' => 'md'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive-md"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - large' => [
+        ['responsive' => 'lg'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive-lg"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - extra large' => [
+        ['responsive' => 'xl'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive-xl"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - extra extra large' => [
+        ['responsive' => 'xxl'],
+        '<table><tr><td>Lorem</td><td>Ipsum</td></tr></table>',
+        '<div class="table-responsive-xxl"><table class="table"><tr><td>Lorem</td><td>Ipsum</td></tr></table></div>',
+      ],
+      'Responsive - nested table' => [
+        ['responsive' => 'always'],
+        '<table><tr><td>Lorem</td><td><table><tr><td>Nested</td><td>Table</td></tr></table></td></tr></table>',
+        '<div class="table-responsive"><table class="table"><tr><td>Lorem</td><td><table class="table"><tr><td>Nested</td><td>Table</td></tr></table></td></tr></table></div>',
+      ],
+    ];
+  }
+
+}
