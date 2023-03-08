f8969bc48ecf16028cec7c0e2f51333c4d6134ed
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2023-03-08 12:29:43 +0200

    OEL-2251: Added title link parameter for gallery and featured plus tests.

--- tests/src/PatternAssertion/GalleryPatternAssert.php
+++ tests/src/PatternAssertion/GalleryPatternAssert.php
@@ -26,6 +26,9 @@ class GalleryPatternAssert extends BasePatternAssert {
       'title_tag' => [
         [$this, 'assertTitleTag'],
       ],
+      'title_url' => [
+        [$this, 'assertTitleLink'],
+      ],
     ];
   }
 
@@ -66,6 +69,18 @@ class GalleryPatternAssert extends BasePatternAssert {
    */
   protected function assertTitleTag(string $expected, Crawler $crawler): void {
     $this->assertElementExists($expected . '.mb-4.bcl-heading', $crawler);
+  }
+
+  /**
+   * Checks the link used for the title.
+   *
+   * @param string $expected
+   *   The expected tag.
+   * @param \Symfony\Component\DomCrawler\Crawler $crawler
+   *   The DomCrawler where to check the element.
+   */
+  protected function assertTitleLink(string $expected, Crawler $crawler): void {
+    $this->assertElementExists('.mb-4.bcl-heading > a[href="' . $expected . '"]', $crawler);
   }
 
   /**
