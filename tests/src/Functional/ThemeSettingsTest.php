5680bc33ce1fe9a386b662c2c38e358d2cde32c6
drishu <aszilagyi@live.com>
2023-05-31 10:51:21 +0300

    OEL-2266: Cover new settings by test.

--- tests/src/Functional/ThemeSettingsTest.php
+++ tests/src/Functional/ThemeSettingsTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_bootstrap_theme\Functional;
 
 use Drupal\Tests\BrowserTestBase;
+use Symfony\Component\DomCrawler\Crawler;
 
 /**
  * Tests the theme settings.
@@ -73,6 +74,64 @@ class ThemeSettingsTest extends BrowserTestBase {
   }
 
   /**
+   * Tests the backwards compatibility theme settings.
+   */
+  public function testBackwardsCompatibilitySettings() {
+    $user = $this->drupalCreateUser([
+      'access administration pages',
+      'administer themes',
+    ]);
+    $this->drupalLogin($user);
+
+    $this->drupalGet('/admin/appearance/settings/oe_bootstrap_theme');
+    $assert_session = $this->assertSession();
+
+    $build = [
+      '#type' => 'pattern',
+      '#id' => 'card',
+      '#variant' => 'search',
+      '#fields' => [
+        'title' => 'Test title',
+        'subtitle' => 'Test subtitle',
+        'text' => 'This is a text example.',
+        'image' => [
+          'src' => 'image_source',
+          'alt' => 'image_alt',
+        ],
+        'content' => '<p>this is the test content</p>',
+      ],
+    ];
+
+    $crawler = $this->getCrawlerFromBuild($build);
+    $node = $crawler->filter('img.d-none.d-md-block');
+    $this->assertCount(0, $node);
+    $node = $crawler->filter('.col-md-3.col-lg-2.rounded.mw-listing-img');
+    $this->assertCount(0, $node);
+    $node = $crawler->filter('.col-md-9.col-lg-10');
+    $this->assertCount(0, $node);
+
+    // Assert card_search_image_not_visible_on_mobile.
+    $checkbox = $assert_session->fieldExists('Card image not visible on mobile');
+    $checkbox->check();
+    $assert_session->buttonExists('Save configuration')->press();
+
+    $crawler = $this->getCrawlerFromBuild($build);
+    $image = $crawler->filter('img.d-none.d-md-block');
+    $this->assertCount(1, $image);
+
+    // Assert card_search_use_grid_classes.
+    $checkbox = $assert_session->fieldExists('Card to use grid classes');
+    $checkbox->check();
+    $assert_session->buttonExists('Save configuration')->press();
+
+    $crawler = $this->getCrawlerFromBuild($build);
+    $node = $crawler->filter('.col-md-3.col-lg-2.rounded.mw-listing-img');
+    $this->assertCount(1, $node);
+    $node = $crawler->filter('.col-md-9.col-lg-10');
+    $this->assertCount(1, $node);
+  }
+
+  /**
    * Renders a table for test purposes.
    *
    * @return string
@@ -92,6 +151,25 @@ class ThemeSettingsTest extends BrowserTestBase {
 
     $html = (string) $this->container->get('renderer')->renderRoot($build);
     return trim(preg_replace('/>\s+</', '><', $html));
+  }
+
+  /**
+   * Gets a crawled build.
+   *
+   * @param array $build
+   *   The build.
+   *
+   * @return \Symfony\Component\DomCrawler\Crawler
+   *   The crawler.
+   */
+  protected function getCrawlerFromBuild(array $build): Crawler {
+    // Theme settings config is cached statically.
+    drupal_static_reset();
+    \Drupal::configFactory()->clearStaticCache();
+
+    $html = (string) $this->container->get('renderer')->renderRoot($build);
+
+    return new Crawler($html);
   }
 
 }
