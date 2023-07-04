7bdc63ec31e9601b53b5f371dc1c0d6c2bb2127a
Francesco Sardara <francesco@tdgwebservices.com>
2023-07-04 18:02:28 +0200

    OEL-2266: Add explicative text.

--- theme-settings.php
+++ theme-settings.php
@@ -47,21 +47,22 @@ function oe_bootstrap_theme_form_system_theme_settings_alter(&$form, FormStateIn
   $form['oebt_bc'] = [
     '#type' => 'details',
     '#title' => t('Backwards compatibility'),
+    '#description' => t('The following settings allow to keep the backward compatibility with previous releases of <em>oe_bootstrap_theme</em>. When turning them off, please check that your code is updated accordingly. All the settings introduced since the theme installation will be on by default.'),
     '#open' => TRUE,
     '#tree' => TRUE,
   ];
 
   $form['oebt_bc']['card_search_image_not_visible_on_mobile'] = [
     '#type' => 'checkbox',
-    '#title' => t('Card image not visible on mobile'),
-    '#description' => t('Card search variant did not show image on mobile, check this to keep the behaviour.'),
+    '#title' => t('Card image hidden on mobile'),
+    '#description' => t('The search variant of the card pattern did not show image on mobile.'),
     '#default_value' => BackwardsCompatibility::getSetting('card_search_image_not_visible_on_mobile'),
   ];
 
   $form['oebt_bc']['card_search_use_grid_classes'] = [
     '#type' => 'checkbox',
     '#title' => t('Card to use grid classes'),
-    '#description' => t('Card search variant used grid classes to structure its content left-right, this changed to col-12 and cl-card-start-col combination, this has an impact on the column sizes. Check this to keep the old behaviour.'),
+    '#description' => t('Card search variant used grid classes to structure its content left-right, this changed to col-12 and cl-card-start-col combination, this has an impact on the column sizes.'),
     '#default_value' => BackwardsCompatibility::getSetting('card_search_use_grid_classes'),
   ];
 }
