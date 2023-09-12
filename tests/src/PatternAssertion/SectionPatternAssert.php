45265baad6ad32bcf36d5830e9eb194d83c0697e
Francesco Sardara <francesco@tdgwebservices.com>
2023-09-12 19:46:23 +0200

    OEL-2502: Fix tests.

--- tests/src/PatternAssertion/SectionPatternAssert.php
+++ tests/src/PatternAssertion/SectionPatternAssert.php
@@ -16,9 +16,17 @@ class SectionPatternAssert extends BasePatternAssert {
    */
   protected function getAssertions(string $variant): array {
     return [
+      'heading' => [
+        [$this, 'assertElementNormalisedHtml'],
+        '.section__title',
+      ],
+      'content' => [
+        [$this, 'assertElementNormalisedHtml'],
+        '.section__body',
+      ],
       'tag' => [
         [$this, 'assertElementTag'],
-        '.section__body',
+        '.section',
       ],
       'heading_tag' => [
         [$this, 'assertElementTag'],
@@ -40,14 +48,36 @@ class SectionPatternAssert extends BasePatternAssert {
    * {@inheritdoc}
    */
   protected function assertBaseElements(string $html, string $variant): void {
-    // Verify that only one SVG markup has been passed.
-    $this->assertElementExists('.section', new Crawler($html));
+    $crawler = new Crawler($html);
+    $this->assertElementExists('body > .section', $crawler);
+    $this->assertElementExists('.section > .section__body', $crawler);
+  }
+
+  /**
+   * Asserts the rendered html of a particular element.
+   *
+   * @param string|null $expected
+   *   The expected value.
+   * @param string $selector
+   *   The CSS selector to find the element.
+   * @param \Symfony\Component\DomCrawler\Crawler $crawler
+   *   The DomCrawler where to check the element.
+   */
+  protected function assertElementNormalisedHtml(?string $expected, string $selector, Crawler $crawler): void {
+    if (is_null($expected)) {
+      $this->assertElementNotExists($selector, $crawler);
+      return;
+    }
+    $this->assertElementExists($selector, $crawler);
+    $element = $crawler->filter($selector);
+    $html = trim(preg_replace('/\s+/u', ' ', $element->html()));
+    self::assertEquals($expected, $html);
   }
 
   /**
    * Asserts the section pattern attributes.
    *
-   * @param array[] $expected
+   * @param string[] $expected
    *   The expected settings.
    * @param \Symfony\Component\DomCrawler\Crawler $crawler
    *   The crawler.
@@ -61,7 +91,7 @@ class SectionPatternAssert extends BasePatternAssert {
   /**
    * Asserts the section pattern heading attributes.
    *
-   * @param array[] $expected
+   * @param string[] $expected
    *   The expected settings.
    * @param \Symfony\Component\DomCrawler\Crawler $crawler
    *   The crawler.
@@ -75,7 +105,7 @@ class SectionPatternAssert extends BasePatternAssert {
   /**
    * Asserts the section pattern wrapper attributes.
    *
-   * @param array[] $expected
+   * @param string[] $expected
    *   The expected settings.
    * @param \Symfony\Component\DomCrawler\Crawler $crawler
    *   The crawler.
