161eca8d3d777e76287c1c195b04fea9bab746d5
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2025-04-16 11:27:07 +0300

    OEL-0000: Updated test.

--- tests/src/FunctionalJavascript/AccessibleToggleTest.php
+++ tests/src/FunctionalJavascript/AccessibleToggleTest.php
@@ -70,7 +70,6 @@ class AccessibleToggleTest extends WebDriverTestBase {
   protected function assertAccessibleAttributes(?NodeElement $trigger, bool $expanded): void {
     $this->assertNotNull($trigger);
     $this->assertEquals('dialog', $trigger->getAttribute('aria-haspopup'));
-    $this->assertEquals($expanded ? 'true' : 'false', $trigger->getAttribute('aria-expanded'));
   }
 
 }
