71e00ecffd4beff799a8a4b8a9044ecfd53060c3
Andreas Hennings <andreas@dqxtech.net>
2025-09-06 13:37:47 +0200

    OEL-4016: Place the mega menu in oe_bootstrap_theme, disable the old navigation block.

--- modules/oe_bootstrap_theme_helper/oe_bootstrap_theme_helper.post_update.php
+++ modules/oe_bootstrap_theme_helper/oe_bootstrap_theme_helper.post_update.php
@@ -7,6 +7,9 @@
 
 declare(strict_types=1);
 
+use Drupal\block\Entity\Block;
+use Drupal\oe_bootstrap_theme\ConfigImporter;
+
 /**
  * Uninstall OE Bootstrap Theme Paragraphs module.
  */
@@ -16,3 +19,29 @@ function oe_bootstrap_theme_helper_post_update_00001(array &$sandbox): void {
     \Drupal::service('module_installer')->uninstall(['oe_bootstrap_theme_paragraphs']);
   }
 }
+
+/**
+ * Place the OEL mega menu block.
+ */
+function oe_bootstrap_theme_helper_post_update_00002(): string {
+  if (!\Drupal::moduleHandler()->moduleExists('block')) {
+    return 'No blocks can be placed, because the block module is not installed.';
+  }
+
+  ConfigImporter::importSingle('module', 'oe_bootstrap_theme_helper', '/config/post_updates/00002_megamenu', 'block.block.oe_bootstrap_theme_oelmegamenu');
+
+  $report = 'The new mega menu block was placed in the navigation region for oe_bootstrap_theme.';
+
+  $old_navigation_block = Block::load('oe_bootstrap_theme_mainnavigation');
+  if (!$old_navigation_block || !$old_navigation_block->getTheme() !== 'oe_bootstrap_theme') {
+    return $report . "\nThe old navigation block was not found.";
+  }
+  if (!$old_navigation_block->status()) {
+    return $report . "\nThe old navigation block was already disabled.";
+  }
+
+  $old_navigation_block->setStatus(FALSE)->save();
+
+  return $report . "\nThe old navigation block was disabled.";
+}
+
