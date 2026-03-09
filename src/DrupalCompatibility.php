110fb03d1c60a930d0afc734f172abdb92d75fe8
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-03-09 17:25:58 +0200

    OEL-4511: Avoid direct ThemeSettingsProvider class reference in compatibility layer.

--- src/DrupalCompatibility.php
+++ src/DrupalCompatibility.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace Drupal\oe_bootstrap_theme;
 
 use Drupal\Component\Utility\DeprecationHelper;
-use Drupal\Core\Extension\ThemeSettingsProvider;
 
 /**
  * Handles Drupal core compatibility behaviors.
  */
 final class DrupalCompatibility {
+
+  /**
+   * Service id for the Drupal 11.3+ theme settings provider.
+   */
+  private const THEME_SETTINGS_PROVIDER_SERVICE = 'Drupal\\Core\\Extension\\ThemeSettingsProvider';
 
   /**
    * Core version where ThemeSettingsProvider::getSetting() should be used.
@@ -30,7 +34,7 @@ final class DrupalCompatibility {
     return DeprecationHelper::backwardsCompatibleCall(
       \Drupal::VERSION,
       self::THEME_SETTINGS_PROVIDER_VERSION,
-      fn () => \Drupal::service(ThemeSettingsProvider::class)->getSetting($setting),
+      fn () => \Drupal::service(self::THEME_SETTINGS_PROVIDER_SERVICE)->getSetting($setting),
       fn () => theme_get_setting($setting),
     );
   }
