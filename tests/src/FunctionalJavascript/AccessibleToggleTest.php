5148a2261da18132c389f3287678a7fd3723b674
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-04-29 10:55:10 +0300

    OEL-0000: Fix accessible toggle tests.

--- tests/src/FunctionalJavascript/AccessibleToggleTest.php
+++ tests/src/FunctionalJavascript/AccessibleToggleTest.php
@@ -34,27 +34,37 @@ class AccessibleToggleTest extends WebDriverTestBase {
   public function testAccessibleToggleAttributes(): void {
     $this->drupalLogin($this->drupalCreateUser([], NULL, TRUE));
     $this->drupalGet('/admin/appearance/ui/patterns');
+    $assert = $this->assertSession();
 
-    $modalTrigger = $this->getSession()->getPage()->find('css', '[data-bs-toggle="modal"]');
-    $offcanvasTrigger = $this->getSession()->getPage()->find('css', '[data-bs-toggle="offcanvas"]');
+    $modalSelector = '[data-bs-toggle="modal"]';
+    $offcanvasSelector = '[data-bs-toggle="offcanvas"]';
 
-    $this->assertAccessibleAttributes($modalTrigger, expanded: FALSE);
+    $modalTrigger = $assert->waitForElementVisible('css', "{$modalSelector}[aria-haspopup=\"dialog\"]");
+    $offcanvasTrigger = $assert->waitForElementVisible('css', "{$offcanvasSelector}[aria-haspopup=\"dialog\"]");
+
+    $this->assertAccessibleAttributes($modalTrigger);
     $this->assertAccessibleAttributes($offcanvasTrigger, expanded: FALSE);
 
-    $modalTrigger->click();
+    $this->clickWhenInViewport($modalSelector);
+    $assert->waitForElementVisible('css', '.modal.show');
+    $modalTrigger = $assert->waitForElement('css', "{$modalSelector}[aria-expanded=\"true\"]");
     $this->assertAccessibleAttributes($modalTrigger, expanded: TRUE);
     $this->assertAccessibleAttributes($offcanvasTrigger, expanded: FALSE);
 
-    $this->getSession()->getPage()->find('css', '.modal .btn-close')->click();
+    $this->clickWhenInViewport('.modal.show .btn-close');
+    $modalTrigger = $assert->waitForElement('css', "{$modalSelector}[aria-expanded=\"false\"]");
+    $this->assertTrue($assert->waitForElementRemoved('css', '.modal-backdrop'));
     $this->assertAccessibleAttributes($modalTrigger, expanded: FALSE);
     $this->assertAccessibleAttributes($offcanvasTrigger, expanded: FALSE);
 
-    $offcanvasTrigger->click();
+    $this->clickWhenInViewport($offcanvasSelector);
+    $assert->waitForElementVisible('css', '.offcanvas.show');
+    $offcanvasTrigger = $assert->waitForElement('css', "{$offcanvasSelector}[aria-expanded=\"true\"]");
     $this->assertAccessibleAttributes($modalTrigger, expanded: FALSE);
     $this->assertAccessibleAttributes($offcanvasTrigger, expanded: TRUE);
 
-    // Close offcanvas and check attribute updates.
-    $this->getSession()->getPage()->find('css', '.offcanvas-backdrop')->click();
+    $this->clickWhenInViewport('.offcanvas-backdrop');
+    $offcanvasTrigger = $assert->waitForElement('css', "{$offcanvasSelector}[aria-expanded=\"false\"]");
     $this->assertAccessibleAttributes($modalTrigger, expanded: FALSE);
     $this->assertAccessibleAttributes($offcanvasTrigger, expanded: FALSE);
   }
@@ -64,12 +74,47 @@ class AccessibleToggleTest extends WebDriverTestBase {
    *
    * @param \Behat\Mink\Element\NodeElement|null $trigger
    *   The trigger element.
-   * @param bool $expanded
+   * @param bool|null $expanded
    *   The expected state of aria-expanded attribute.
    */
-  protected function assertAccessibleAttributes(?NodeElement $trigger, bool $expanded): void {
+  protected function assertAccessibleAttributes(?NodeElement $trigger, ?bool $expanded = NULL): void {
     $this->assertNotNull($trigger);
     $this->assertEquals('dialog', $trigger->getAttribute('aria-haspopup'));
+    if ($expanded !== NULL) {
+      $this->assertEquals($expanded ? 'true' : 'false', $trigger->getAttribute('aria-expanded'));
+    }
+  }
+
+  /**
+   * Scrolls an element into the viewport and clicks it.
+   *
+   * @param string $selector
+   *   The CSS selector for the element.
+   */
+  protected function clickWhenInViewport(string $selector): void {
+    $encodedSelector = json_encode($selector, JSON_THROW_ON_ERROR);
+    $this->getSession()->executeScript(<<<JS
+      (function () {
+        var element = document.querySelector($encodedSelector);
+        if (!element) {
+          throw new Error('Element not found: ' + $encodedSelector);
+        }
+        element.scrollIntoView({block: 'center'});
+      }());
+    JS);
+    $this->assertTrue($this->getSession()->wait(5000, <<<JS
+      (function () {
+        var element = document.querySelector($encodedSelector);
+        if (!element) {
+          return false;
+        }
+        var rect = element.getBoundingClientRect();
+        return rect.top >= 0 && rect.left >= 0 &&
+          rect.bottom <= window.innerHeight &&
+          rect.right <= window.innerWidth;
+      }())
+    JS));
+    $this->assertSession()->elementExists('css', $selector)->click();
   }
 
 }
