8d413f4f4583dae5837696c1b2ae30110d8ea56e
Tiberiu Dumitru <tiberiu.dumitru23@gmail.com>
2026-05-20 11:20:04 +0300

    OEL-4746: Added more files to check.

--- tests/src/Unit/ThemeAssetsTest.php
+++ tests/src/Unit/ThemeAssetsTest.php
@@ -17,10 +17,16 @@ class ThemeAssetsTest extends UnitTestCase {
   public function testCanaryAssetFilesExist(): void {
     $theme_root = dirname(__DIR__, 3);
     $expected_files = [
+      'assets/bcl/bcl-button/bcl-button.html.twig',
+      'assets/css/oe_bootstrap_theme.style.min.css',
       'assets/logos/ec/logo-ec--en.svg',
       'assets/logos/ec/logo-ec--fr.svg',
       'assets/logos/eu/logo-eu--en.svg',
       'assets/logos/eu/mobile/logo-eu--en.svg',
+      'node_modules/@openeuropa/bcl-theme-default/css/oe-bcl-default.min.css',
+      'node_modules/@openeuropa/bcl-theme-default/logos/ec/positive/logo-ec--en.svg',
+      'node_modules/@openeuropa/bcl-theme-default/logos/eu/standard-version/positive/logo-eu--en.svg',
+      'node_modules/@openeuropa/bcl-theme-default/templates/bcl-button/button.html.twig',
     ];
 
     $missing_files = array_values(array_filter(
