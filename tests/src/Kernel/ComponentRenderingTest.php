11d11d70fbdbaeb5045fc71b2b299d9a722eac00
Piotr Smykaj <piotr.smykaj@gmail.com>
2026-06-16 11:35:52 +0200

    OEL-4814: Add accordion component.

--- tests/src/Kernel/ComponentRenderingTest.php
+++ tests/src/Kernel/ComponentRenderingTest.php
@@ -36,6 +36,7 @@ class ComponentRenderingTest extends KernelTestBase implements FormInterface {
    * @var string[]
    */
   private static $componentList = [
+    'accordion',
     'alert',
     'badge',
     'banner',
