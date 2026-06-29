869b902d5658126635651005832250e5a014a190
Dumitru Tiberiu-Andrei <Tiberiu-Andrei.DUMITRU@ext.ec.europa.eu>
2026-06-29 12:59:41 +0300

    OEL-4880: Port Card v2 to SDC.

--- tests/src/Kernel/ComponentRenderingTest.php
+++ tests/src/Kernel/ComponentRenderingTest.php
@@ -35,6 +35,7 @@ class ComponentRenderingTest extends KernelTestBase implements FormInterface {
    * @var string[]
    */
   private static $componentList = [
+    'card_v2',
     'description_list',
   ];
 
