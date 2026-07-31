a388570125dfb57b86bda6e7bce9367ef923acc7
Piotr Smykaj <piotr.smykaj@gmail.com>
2026-07-31 17:08:06 +0200

    OEL-4927: Port File pattern into SDC.

--- tests/src/Kernel/ComponentRenderingTest.php
+++ tests/src/Kernel/ComponentRenderingTest.php
@@ -53,6 +53,7 @@ class ComponentRenderingTest extends KernelTestBase implements FormInterface {
     'dropdown',
     'fact_figures',
     'featured_media',
+    'file',
     'gallery',
     'progress',
     'timeline',
